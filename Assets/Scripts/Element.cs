fd6fe6d085f4bfbb599756ce308f92c355023d39
Arsenij67 <96657896+Arsenij67@users.noreply.github.com>
2023-03-15 20:30:52 +0300

    Рабочий скрипт для 2х первых элементов

--- Assets/Scripts/Element.cs
+++ Assets/Scripts/Element.cs
@@ -8,37 +8,33 @@ public class Element : MonoBehaviour
 
     private static List<Element> _elements = new List<Element>();
 
+   
     public bool isSelected = false;
+
+    public GameObject select;
+
+    private void Awake()
+    {
+        select = GameObject.Find("selected");
+    }
     public void Select()
     {
-        
+         
 
-
-        if(_elements.Count < 1)
+        _elements.Clear();
         _elements.AddRange(GameObject.FindObjectsOfType<Element>());
+
+
+
+        select.transform.parent = gameObject.transform;
+
+        select.transform.localPosition = new Vector3(-30,-70,0);
 
 
         Debug.Log("количество товаров - " + _elements.Count);
 
-        GameObject Childelement = gameObject.transform.GetChild(0).gameObject;
-
-    
-    
-
-
-        foreach (var elem in _elements)
-        {
-
-
-             elem.isSelected = false;
-            if(elem.gameObject.transform.childCount > 0)
-             elem.gameObject.transform?.GetChild(0)?.gameObject.SetActive(false);
-        
-        
-        }
-
-        Childelement.SetActive(true);
-        isSelected = true;
+      
+        //isSelected = true;
 
 
 
