3ee4748a76ce137b9a85bdb0ccaf6a4b6531d814
Arsenij67 <96657896+Arsenij67@users.noreply.github.com>
2023-03-15 21:01:11 +0300

    Финалочка

--- Assets/Scripts/Element.cs
+++ Assets/Scripts/Element.cs
@@ -8,32 +8,34 @@ public class Element : MonoBehaviour
 
     private static List<Element> _elements = new List<Element>();
 
-   
+
     public bool isSelected = false;
 
     public GameObject select;
 
+
     private void Awake()
     {
-        select = GameObject.Find("selected");
+        select = GameObject.FindGameObjectWithTag("Selected");
     }
     public void Select()
     {
-         
+
+
 
         _elements.Clear();
         _elements.AddRange(GameObject.FindObjectsOfType<Element>());
 
 
 
-        select.transform.parent = gameObject.transform;
+        select.transform.SetParent(this.gameObject.transform);
 
-        select.transform.localPosition = new Vector3(-30,-70,0);
+        select.transform.localPosition = new Vector3(-30, -70, 0);
 
 
         Debug.Log("количество товаров - " + _elements.Count);
 
-      
+
         //isSelected = true;
 
 
